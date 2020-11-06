796cf5e0098e6f7ff7f3d34c1b46f2da1c5fe38e
longxiangfu <1565267514@qq.com>
2020-11-06 16:38:59 +0800

    io操作

--- src/main/java/com/lxf/jdk/foundation/io/NIO/completeDemo/Client.java
+++ src/main/java/com/lxf/jdk/foundation/io/NIO/completeDemo/Client.java
@@ -22,9 +22,11 @@ public class Client {
         try {
             //打开通道
             socketChannel = SocketChannel.open();
+            //设置通道道为非阻塞模式
+//            socketChannel.configureBlocking(false);
             //进行连接
             socketChannel.connect(address);
-            String request = "我是小王";
+            String request = "非阻塞模式下,read()方法在尚未读取到任何数据时可能就返回了。所以需要关注它的int返回值，它会告诉你读取了多少字节";
             byte[] bytes = request.getBytes();
             //把数据放到缓冲区中
             buf.put(bytes);
@@ -47,32 +49,21 @@ public class Client {
         try {
             //1 清空缓冲区旧的数据
             readBuf.clear();
-            boolean isRead = false;
-            for (int i = 0; i < 3; i++) {
-                readBuf.clear();
-                int count = socketChannel.read(readBuf);
-                //4 如果没有数据
-                if (count == -1) {
-                    if (i == 2) {
-                        socketChannel.close();
-                    }
-                }else {
-                    isRead = true;
-                    break;
-                }
+            int count = socketChannel.read(readBuf);
+            //4 如果没有数据
+            if (count == -1) {
+                return;
             }
-            if (isRead) {
-                //5 有数据则进行读取 读取之前需要进行复位方法(把position 和limit进行复位)
-                readBuf.flip();
-                //6 根据缓冲区的数据长度创建相应大小的byte数组，接收缓冲区的数据
-                byte[] bytes = new byte[readBuf.remaining()];
-                //7 接收缓冲区数据
-                readBuf.get(bytes);
-                //8 打印结果
-                String body = new String(bytes).trim();
-                System.out.println("Client receive:" + body);
-            }
-        } catch (IOException e) {
+            //5 有数据则进行读取 读取之前需要进行复位方法(把position 和limit进行复位)
+            readBuf.flip();
+            //6 根据缓冲区的数据长度创建相应大小的byte数组，接收缓冲区的数据
+            byte[] bytes = new byte[readBuf.remaining()];
+            //7 接收缓冲区数据
+            readBuf.get(bytes);
+            //8 打印结果
+            String body = new String(bytes).trim();
+            System.out.println("Client receive:" + body);
+    } catch (IOException e) {
             e.printStackTrace();
         }
 
--- src/main/java/com/lxf/jdk/foundation/io/NIO/completeDemo/Server.java
+++ src/main/java/com/lxf/jdk/foundation/io/NIO/completeDemo/Server.java
@@ -3,7 +3,10 @@ package com.lxf.jdk.foundation.io.NIO.completeDemo;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.util.Iterator;
 
@@ -11,7 +14,7 @@ public class Server implements Runnable {
     //1 多路复用器（管理所有的通道）
     private Selector seletor;
     //2 建立读缓冲区
-    private ByteBuffer readBuf = ByteBuffer.allocate(1024);
+    private ByteBuffer readBuf = ByteBuffer.allocate(5);
 
 
     public Server(int port){
@@ -72,37 +75,26 @@ public class Server implements Runnable {
             //2 获取之前注册的socket通道对象
             SocketChannel socketChannel = ((ServerSocketChannel)key.channel()).accept();
             //3 循环读取数据
-            boolean isRead = false;
-            for (int i = 0; i < 3; i++) {
-                this.readBuf.clear();
-                int count = socketChannel.read(this.readBuf);
-                //4 如果没有数据
-                if(count == -1){
-                    if(i == 2){
-                        key.channel().close();
-                        key.cancel();
-                    }
-                }else {
-                    isRead = true;
-                    break;
-                }
+            int count = socketChannel.read(this.readBuf);
+            //4 如果没有数据
+            if(count == -1){
+                key.channel().close();
+                key.cancel();
+                return;
             }
-            if (isRead) {
-                //5 有数据则进行读取 读取之前需要进行复位方法(把position 和limit进行复位)
-                this.readBuf.flip();
-                //6 根据缓冲区的数据长度创建相应大小的byte数组，接收缓冲区的数据
-                byte[] bytes = new byte[this.readBuf.remaining()];
-                //7 接收缓冲区数据
-                this.readBuf.get(bytes);
-                //8 打印结果
-                String body = new String(bytes).trim();
-                System.out.println("Server receive: " + body);
-                // 9 写回给客户端数据
-//                //写出数据
-//                String response = "老王，您好！";
-//                socketChannel.write(Charset.defaultCharset().encode(response));
-//                System.out.println("Server write:" + response);
-            }
+            //5 有数据则进行读取 读取之前需要进行复位方法(把position 和limit进行复位)
+            this.readBuf.flip();
+            //6 根据缓冲区的数据长度创建相应大小的byte数组，接收缓冲区的数据
+            byte[] bytes = new byte[this.readBuf.remaining()];
+            //7 接收缓冲区数据
+            this.readBuf.get(bytes);
+            //8 打印结果
+            String body = new String(bytes).trim();
+            System.out.println("Server receive: " + body);
+            // 9 写回给客户端数据
+            String response = "老王，您好！";
+            socketChannel.write(Charset.defaultCharset().encode(response));
+            System.out.println("Server write:" + response);
         } catch (IOException e) {
             e.printStackTrace();
         }
